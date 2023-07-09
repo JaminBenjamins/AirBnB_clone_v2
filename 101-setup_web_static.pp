72cbc62b1f6fcf7f4513c7de5ef66246ef2af65e
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-09 20:42:55 +0300

    Setting up a web server using Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -7,13 +7,16 @@ $nginx_conf = "server {
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
         return 301 http://linktr.ee/firdaus_h_salim/;
     }
+
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
