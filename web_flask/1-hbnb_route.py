940082a1207ae654bf20831b1757d37e8bb7cc55
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-21 22:21:49 +0300

    Defining routes for a flask application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A script that starts a web application with given routes"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Prints given string"""
+    return "Hello HBNB!"
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
