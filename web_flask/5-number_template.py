2a7ca9b1934ac589d60ae07e8813c119cc4b4dfc
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-22 18:24:27 +0300

    Html file to print if n is a number

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -40,7 +40,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def num_html(n):
     """display html if n is a number"""
-    return render_template("5-number.html", name=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
