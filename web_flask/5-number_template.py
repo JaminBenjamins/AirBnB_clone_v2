b9ccc8ce4c41f6846d9509443a58ca89b2b76492
JaminBenjamins <jaminbenjamins@gmail.com>
2023-07-22 18:00:55 +0300

    Showing an html document if gicen value is int

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""A script that starts a web application with given routes"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Prints given string"""
+    return "Hello HBNB!"
+
+
+@app.route("/", strict_slashes=False)
+def hbnb():
+    """Display 'HBNB' """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Print given text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display(text):
+    """Display Python {}" followed by given input"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display n is a number if n is an integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_html(n):
+    """display html if n is a number"""
+    return render_template("5-number.html", name=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
